909d4634b8cb64f07e874bf1bcf122fdaec6cbb9
4thdraw <4thdraw2@gmail.com>
2023-07-05 18:02:17 +0900

    리액트 훅 useState가이드

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Rcom from "./Return"
-
+import { useState } from "react";
+import './App.css'
 function App() {
 
-  return (
-   <>
-     
-        
-   </>
+  const bgcolor = ['red','green','blue'] 
+  const [colovar, setcolor] = useState(0)
+  //setcolor(2)   ->  colovar =2 
+  return (    
+    <button className={bgcolor[colovar]}
+
+            onClick={()=>{ 
+              setcolor( (colovar + 1 ) % bgcolor.length ) //colovar ->1                              
+              }}>
+              {              
+              document.body.classList =  bgcolor[colovar] 
+              }              
+    </button>  
   );
 }
 
